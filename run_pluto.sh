2b8e1052feb88b9c4eec4141c39857a9243a9d50
daniel <>
2023-04-21 18:18:49 +0000

    Solo instancia si Manifest.toml no existe
    FossilOrigin-Name: 8cc2e23afff4294d17b7866744908112e4379c978acd2d3e17b710d1a0465fc3

--- run_pluto.sh
+++ run_pluto.sh
@@ -1,2 +1,7 @@
 #!/bin/env sh
-julia --project=. -e 'using Pkg; Pkg.instantiate(); using Pluto; Pluto.run(disable_writing_notebook_files=true)'
+export JULIA_PROJECT=.
+
+if [ ! -e Manifest.toml ]; then
+   julia --project=. -e 'using Pkg; Pkg.instantiate()'
+fi
+julia --project=. -e 'using Pluto; Pluto.run(disable_writing_notebook_files=true)'
