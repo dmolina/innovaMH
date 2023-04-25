4be9d75730b6b0717e3d38695395e0b79efe0b0a
daniel <>
2023-04-25 19:08:21 +0000

    Add instantiate
    FossilOrigin-Name: 037c1aa4a7f86f03b25610605e551c1004d51588f9eadc6c67df68ad4d00aabc

--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
 #!/bin/env sh
+export JULIA_PROJECT=.
+
+if [ ! -e Manifest.toml ]; then
+    julia --project=. -e 'using Pkg; Pkg.instantiate()'
+fi
 julia --project=. -e 'using PlutoSliderServer; PlutoSliderServer.run_directory(".", SliderServer_port=8100, SliderServer_host="0.0.0.0", Export_offer_binder=true, Export_disable_ui=true)'
